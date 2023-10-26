3453a62546005e8a9f9d19965481a1ca0ca533c6
Muhammad Osman <139002050+957637367576@users.noreply.github.com>
2023-10-27 02:01:22 +0500

    changes some colors

--- script.js
+++ script.js
@@ -48,6 +48,7 @@ const songs=[
 }
 ];
 
+
 //index of array
 var index = 0;
 
@@ -55,7 +56,7 @@ var index = 0;
 var image = document.querySelector(".thumb-image");
 var song_name = document.querySelector(".song-name");
 var song = document.querySelector(".songs");
-
+var loop = document.querySelector("#loop");
 //control-panel variables
 var progress = document.querySelector(".progress-bar");
 var fwbtn = document.querySelector("#fw");
@@ -70,7 +71,6 @@ song.src = songs[index].link;
 //when song is loaded then this function is called
 song.onloadedmetadata = function(){
     progress.max = song.duration;
-    progress.value = song.currentTime;
 };
 
 //condition for play button
@@ -95,22 +95,36 @@ if(song.play())
 {
     setInterval(()=>{
         progress.value = song.currentTime;
-        if(song.duration == song.currentTime)
+        if(song.duration === song.currentTime)
         {
-        index++;
+        if(index === 7)
+        {
+        index = 0;
         image.src = songs[index].image_src;
         song_name.innerHTML = songs[index].name;
         song.src = songs[index].link;
-        song.play();   
+        song.play();
         }
+    }
+        if(song.duration === song.currentTime)
+        {
+        index ++;
+        image.src = songs[index].image_src;
+        song_name.innerHTML = songs[index].name;
+        song.src = songs[index].link;
+        song.play();
+        
+        }
+      
     },1000);
-}
+
+}  
 
 progress.onchange = function () {
     song.play();
     song.currentTime = progress.value;
   };
-  
+
 
 //forward or backward
 fwbtn.addEventListener("click",function(){
@@ -142,16 +156,29 @@ fwbtn.addEventListener("click",function(){
             song_name.innerHTML = songs[index].name;
             song.src = songs[index].link;
             song.play();
+            playbtn.classList.add("fa-circle-pause");
         }
+
         else
         {
             image.src = songs[index].image_src;
             song_name.innerHTML = songs[index].name;
             song.src = songs[index].link;
             song.play();
+            playbtn.classList.add("fa-circle-pause");
         }
+        
     
     });
+
+    progress.addEventListener('click',function(){
+        playbtn.classList.add("fa-circle-pause");
+    })
+
+loop.addEventListener('click',function(){
+    song.loop = true;
+})
+
 
     //Dark or Light Mode
 var body = document.querySelector("body");
@@ -161,47 +188,6 @@ var button2 = document.querySelector("#btn2");
 var h2 = document.querySelector("h2");
 var btn1 = document.querySelector(".btn1");
 var btn2 = document.querySelector(".btn2");
-function lightmode(){
-    body.style.backgroundColor = "white"
-    button2.style.border = "solid 1.5px black"
-    button2.style.color = "black"
-    button2.style.height = "45px"
-    button2.style.width = "170px"
-    button2.style.fontSize = "18px"
-    button2.style.borderRadius = "50px"
-    main.style.backgroundColor = "white"
-    main.style.color = "black"
-    fwbtn.style.color = "black"
-    bwbtn.style.color = "black"
-    playbtn.style.color = "black"
-    h2.style.color = "black"
-    song_name.style.color = "black"
-    progress.style.backgroundColor = "black"
-    button2.style.cursor = "pointer"
-}
-function darkmode(){
-    button1.style.border = "solid 1.5px white"
-    button1.style.color = "white"
-    button1.style.height = "45px"
-    button1.style.width = "170px"
-    button1.style.fontSize = "18px"
-    button1.style.borderRadius = "50px"
-    body.style.backgroundColor = "black"
-    body.style.color = "black"
-    main.style.backgroundColor = "black"
-    main.style.color = "white"
-    fwbtn.style.color = "white"
-    bwbtn.style.color = "white"
-    playbtn.style.color = "white"
-    h2.style.color = "white"
-    song_name.style.color = "white"
-    progress.style.backgroundColor = "white"
-    button2.style.cursor = "pointer"
-}
-
-
-button1.addEventListener("click", lightmode);
-button2.addEventListener("click",darkmode)
 
 
 // Mouse Follower
